f3aa6e464f4887a4c9701046bffd49fd47b0a73c
Yasen Ivanov <yasen.denchev@gmail.com>
2024-05-10 17:47:57 +0300

    add lazy loading and remove inline styles for device images

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,52 @@
 /*!
-* Start Bootstrap - New Age v6.0.7 (https://startbootstrap.com/theme/new-age)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
-*/
+ * Start Bootstrap - New Age v6.0.7 (https://startbootstrap.com/theme/new-age)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
+ */
 //
 // Scripts
 //
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Activate Bootstrap scrollspy on the main nav element
+  const mainNav = document.body.querySelector("#mainNav");
+  if (mainNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#mainNav",
+      offset: 74,
+    });
+    window.addEventListener("scroll", (event) => {
+      if (window.scrollY > 60) {
+        mainNav.classList.add("navbar-scrolled");
+      } else if (window.scrollY < 60) {
+        mainNav.classList.remove("navbar-scrolled");
+      }
+    });
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
+    // lazy loading for images with class "lazy"
+    var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+    if ("IntersectionObserver" in window) {
+      let lazyImageObserver = new IntersectionObserver(function (
+        entries,
+        observer
+      ) {
+        entries.forEach(function (entry) {
+          if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.srcset = lazyImage.dataset.srcset;
+            lazyImage.classList.remove("lazy");
+            lazyImageObserver.unobserve(lazyImage);
+          }
         });
-        window.addEventListener('scroll', event => {
-            if(window.scrollY>60){
-                mainNav.classList.add('navbar-scrolled');
-            } else if (window.scrollY < 60) {
-                mainNav.classList.remove('navbar-scrolled');
-            }
-        });
-    };
+      });
+
+      lazyImages.forEach(function (lazyImage) {
+        lazyImageObserver.observe(lazyImage);
+      });
+    } else {
+      // Possibly fall back to event handlers here
+    }
+  }
 });
